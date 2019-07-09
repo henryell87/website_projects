b643c63658c90baf0ee4d2f3c452d86df1b7d654
Henry Ell <henryell87@gmail.com>
2019-07-09 09:52:09 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    // Program start class
+    class WelcomeCSS
+    {
+        // Main begins program execution
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the C# Tutorial*");
+        }
+    }
+}
